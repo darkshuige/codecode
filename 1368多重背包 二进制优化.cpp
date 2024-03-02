b66054fb9058aaf18323fe798c61b740d69c9dca
darkshuige_lab <1597590247@qq.com>
2024-03-02 13:00:31 +0800

    a

--- 1368多重背包 二进制优化.cpp
+++ 1368多重背包 二进制优化.cpp
@@ -8,18 +8,18 @@ int main()
 		int dp[50005]={0};
 		int vv[50005]={0};
 		int ww[50005]={0};
-		int num=1;
+		int num=1;//拆分计数
 		for(int i=1;i<=n;i++)
 		{
 			int v,w,s;
-			cin>>v>>w>>s;
+			cin>>v>>w>>s;//体积，价值，数量
 			for(int j=1;j<=s;j<<=1)
 			{
 				vv[num]=v*j;
 				ww[num++]=w*j;
 				s-=j;
 			}
-			if(s)
+			if(s)// 剩余
 			{
 				vv[num]=s*v;
 				ww[num++]=s*w;
